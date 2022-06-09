6c8418912b810d1bc6cafe92631d855baf1bb5db
nayun <yuniyun@naver.com>
2022-06-09 20:42:14 +0900

    프로젝트

---
+++ src/team/Teammain.java
@@ -0,0 +1,29 @@
+package team;
+
+import java.util.Scanner;
+
+public class Teammain {
+
+		public static void main(String[] args) {
+			
+			int num;
+			
+			Scanner input = new Scanner(System.in);
+			
+			while(true) {
+			
+				System.out.println("1. 회원관리 이동");
+				System.out.println("2. 이벤트관리 이동");
+				System.out.println("3. 게시판 이동");
+				num = input.nextInt();
+				
+				switch(num) {
+				case 1 : break;
+				case 2 : break;
+				case 3 : break; 
+				
+				}
+			
+			}
+		}
+}
